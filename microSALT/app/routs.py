528a0f096c4eb34a532686db7b229a60ab9edce4
sylvinite <isak.sylvin@scilifelab.se>
2017-11-16 13:28:41 +0100

    Minor integration fix

--- microSALT/app/routs.py
+++ microSALT/app/routs.py
@@ -2,20 +2,21 @@ import os
 import sys
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from bla bla import app
-from blo blo import db, Blast som ska ersättas med alla modellerna
+from microSALT import db
+from microSALT.tables.samples import Samples
+#from microSALT.app import app
 
 @app.route('/microSALT/')
 def start_page():
-    blast = Blast.query.all()
+    blast = Samples.query.all()
     return render_template('start_page.html',
         blast  = blast)
 
 
 @app.route('/microSALT/<sample>')
 def sample_page(sample):
-    blast = Blast.query.filter_by(run=sample)
-    print dir(blast[0])
+    blast = Samples.query.filter_by(CG_ID_sample=sample)
+    #print dir(blast[0])
     return render_template('sample_page.html',
         blast  = blast,
         sample_name = sample,
