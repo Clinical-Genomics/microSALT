effea82d32645551c5b28c97f92d0c901f1dcfa6
sylvinite <isak.sylvin@scilifelab.se>
2017-12-05 18:03:55 +0100

    Added extensive ST search. Needs some tightening

--- microSALT/store/db_manipulator.py
+++ microSALT/store/db_manipulator.py
@@ -170,38 +170,100 @@ class DB_Manipulator:
           self.logger.info("Found loci {}, which has no profile entry".format(loci))
           return 0
 
-  def get_100pc_alleles(self, cg_sid):
-    """ Only grabs alleles from a samples that have 100% hit rate. Solves most problems"""
-    return self.session.query(Seq_types.loci, Seq_types.allele).filter(Seq_types.CG_ID_sample==cg_sid, Seq_types.identity==100).all()
+  def simpleAllele2st(self, cg_sid):
+    """ Finds a definitive ST if such exists """
+    organism = self.session.query(Samples.organism).filter(Samples.CG_ID_sample==cg_sid).scalar()
+    # Find clear top hits
+    hits = self.session.query(Seq_types.loci, Seq_types.allele, Seq_types.identity).filter(Seq_types.CG_ID_sample==cg_sid, Seq_types.identity>=99.9, Seq_types.evalue==0.0).all()
+
+    #Check that there's only correct number of loci hits
+    if len(self.profiles[organism].columns.values()) == len(hits):
+      self.logger.info("Equal number of hits as organism profile for {} on {}".format(cg_sid, organism))
+
+    #Find ST
+    filterstring = ""
+    for entry in hits:
+      filterstring += " {}={} and".format(entry.loci, entry.allele)
+    filterstring = filterstring[:-3]
+    output = self.session.query(self.profiles[organism]).filter(text(filterstring)).all()
+
+    if len(output) == 1:
+      return output[0].ST
+    else:
+      return -1 #No Simple solution found
+
+  def complexAllele2st(self, cg_sid):
+    organism = self.session.query(Samples.organism).filter(Samples.CG_ID_sample==cg_sid).scalar()
+    if 'clonal_complex' in self.profiles[organism].columns.keys():
+      non_allele_columns = 2
+    else:
+      non_allele_columns = 1 
+    #All hits
+    hits = self.session.query(Seq_types.loci, Seq_types.allele, Seq_types.identity).filter(Seq_types.CG_ID_sample==cg_sid).all()
+
+    #Establish there's enough unique hits at all, otherwise we're screwed from the get go
+    uniqueDict = dict()
+    for hit in hits:
+      if hit.loci not in uniqueDict.keys():
+        uniqueDict[hit.loci] = list()
+        uniqueDict[hit.loci].append(hit.allele)
+      elif hit.allele not in uniqueDict[hit.loci]:
+        uniqueDict[hit.loci].append(hit.allele)
+
+    #Check that correct amount of alleles has been found AND that no allele has multiple numbers
+    #This corrects the issue where the loci does not have 100% id
+    if len(self.profiles[organism].columns.values()) - non_allele_columns == len(uniqueDict.keys()) and sum([len(x) for x in uniqueDict.values()]) == len(self.profiles[organism].columns.values()) - non_allele_columns:
+      # Find ST (extended)
+      filterstring = ""
+      for entry in hits:
+        filterstring += " {}={} and".format(entry.loci, entry.allele)
+      filterstring = filterstring[:-3]
+      output = self.session.query(self.profiles[organism]).filter(text(filterstring)).all()
+  
+      if len(output) > 0:
+        self.logger.warning("Sampe {} has ST {} from extended search (breaking thresholds). Be aware".format(cg_sid, output[0].ST))
+        return output[0].ST
+      else:
+        self.logger.warning("Sufficent loci found but no ST. True hit is not top hit, or novel ST has been found. Sample {} on {}. Setting ST to -2".format(cg_sid, organism))
+        return -2
+    elif len(self.profiles[organism].columns.values()) - non_allele_columns > len(uniqueDict.keys()):
+      self.logger.warning("Less hits than organism profile for {} on {}. Relax blast constraint? Unable to fanthom a ST. Setting to -3".format(cg_sid, organism))
+      return -3
+
+    # This corrects the issue where multiple alleles for a given loci were found.
+    # Find all ST combinations, put them in a list
+    filterstring = ""
+    for key, val in uniqueDict.items():
+      for num in val:
+        if val.index(num) == 0 and len(val) > 1:
+          filterstring += " ("
+
+        filterstring += " {}={} ".format(key, num) 
+
+        if len(val) == 1:
+          filterstring += "and"
+        elif val.index(num)+1 == len(val) and len(val) > 1:
+          filterstring += ") and"
+        else:
+          filterstring += "or"
+    filterstring = filterstring[:-3]
+    output = self.session.query(self.profiles[organism]).filter(text(filterstring)).all()
+    if len(output) > 1:
+      STlist= list()
+      for st in output:
+        STlist.append(st.ST) 
+      self.logger.warning("Multiple possible ST found for sample {}, list: {}".format(cg_sid, STlist))
+    else:
+      self.logger.warning("Established ST{} for {} from expandeded search".format(output[0].ST, cg_sid))
+      return output[0].ST
 
   def alleles2st(self, cg_sid):
     """Takes a CG sample ID and calculates most likely ST"""
-    # TODO: Can be cleaned A LOT.
-    organism = self.session.query(Samples.organism).filter(Samples.CG_ID_sample==cg_sid).scalar()
-    #ONLY GRABS ALLELES WITH 100% ID
-    alleles =self.get_100pc_alleles(cg_sid) 
-    #Ugly check that duplicate entries dont exist
-    alle_list = list()
-    for item in alleles:
-      if not item[0] in alle_list:
-        alle_list.append(item[0])
-      else:
-        self.logger.warning("Alles are non-unique for sample {}. Setting ST to -2.".format(cg_sid))
-        return -2
- 
-    for k,v in self.profiles.items():
-      if k == organism:
-        filtero = ""
-        index = 0
-        while index <  len(alleles):
-          filtero += "v.c.{}=={}, ".format(alleles[index][0], alleles[index][1])
-          index += 1
-        ST = eval("self.session.query(v).filter({}).all()".format(filtero))
-        if ST == []:
-          self.logger.warning("No ST for allele combo of sample {} found. Setting ST to 0.".format(cg_sid))
-          return 0
-        if len(ST) > 1:
-          self.logger.warning("Multiple ST found for sample {}. Setting ST to -1".format(cg_sid))
-          return -1
-        else: 
-          return ST[0][0]
+    # All this needs to be conveyed to views
+    ST = self.simpleAllele2st(cg_sid)
+    if ST > 0:
+      return ST
+    #TODO: Distinquish between different negative codes
+    else:
+      self.logger.warning("No Simple ST found for {}. Using complex finder.".format(cg_sid))
+      return self.complexAllele2st(cg_sid)
