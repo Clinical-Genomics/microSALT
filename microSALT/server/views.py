f052af229ce569ced676762032744cda20c78b0e
Isak Sylvin <isak.sylvin@scilifelab.se>
2019-02-03 13:59:05 +0100

    Indentation, blank as control word

--- microSALT/server/views.py
+++ microSALT/server/views.py
@@ -75,7 +75,10 @@ def gen_reportdata(pid, organism_group='all'):
   #Set ST status
   for s in sample_info:
     s.ST_status=str(s.ST)
-    if s.Customer_ID_sample.startswith('NTC') or s.Customer_ID_sample.startswith('0-') or s.Customer_ID_sample.startswith('NK-') or s.Customer_ID_sample.startswith('NEG') or s.Customer_ID_sample.startswith('CTRL') or s.Customer_ID_sample.startswith('Neg'):
+    if s.Customer_ID_sample.startswith('NTC') or s.Customer_ID_sample.startswith('0-') or \ 
+    s.Customer_ID_sample.startswith('NK-') or s.Customer_ID_sample.startswith('NEG') or \
+    s.Customer_ID_sample.startswith('CTRL') or s.Customer_ID_sample.startswith('Neg') or \
+    s.Customer_ID_sample.startswith('blank'):
       s.ST_status = 'Control (prefix)'
     elif s.ST < 0:
       if s.ST == -1:
@@ -94,9 +97,11 @@ def gen_reportdata(pid, organism_group='all'):
       s.threshold = 'Passed'
       for seq_type in s.seq_types:
         #Identify single deviating allele
-        if seq_type.st_predictor and seq_type.identity >= config["threshold"]["mlst_novel_id"] and config["threshold"]["mlst_id"] > seq_type.identity and 1-abs(1-seq_type.span) >= config["threshold"]["mlst_span"]:
+        if seq_type.st_predictor and seq_type.identity >= config["threshold"]["mlst_novel_id"] and \
+        config["threshold"]["mlst_id"] > seq_type.identity and 1-abs(1-seq_type.span) >= config["threshold"]["mlst_span"]:
           near_hits = near_hits + 1
-        elif (seq_type.identity < config["threshold"]["mlst_novel_id"] or seq_type.span < config["threshold"]["mlst_span"]) and seq_type.st_predictor:
+        elif (seq_type.identity < config["threshold"]["mlst_novel_id"] or \
+              seq_type.span < config["threshold"]["mlst_span"]) and seq_type.st_predictor:
           s.threshold = 'Failed'
 
       if near_hits > 0 and s.threshold == 'Passed':
@@ -106,7 +111,8 @@ def gen_reportdata(pid, organism_group='all'):
 
     #Resistence filter
     for r in s.resistances:
-      if (s.ST > 0 or 'Novel' in s.ST_status ) and (r.identity >= config["threshold"]["res_id"] and r.span >= config["threshold"]["res_span"]) or (s.ST < 0 and not 'Novel' in s.ST_status):
+      if (s.ST > 0 or 'Novel' in s.ST_status ) and (r.identity >= config["threshold"]["res_id"] and \
+      r.span >= config["threshold"]["res_span"]) or (s.ST < 0 and not 'Novel' in s.ST_status):
         r.threshold = 'Passed'
       else:
         r.threshold = 'Failed'
