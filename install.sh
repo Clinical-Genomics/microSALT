babff0a1ab8de415fcee3af6009392c25f4c81b6
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2019-07-19 12:38:38 +0200

    Fixed quast installation errors

--- install.sh
+++ install.sh
@@ -39,11 +39,11 @@ conda create -y -n $rname python=3.6
 #source deactivate
 source activate $rname
 conda config --add channels bioconda
-conda install -y -c bioconda blast=2.5.0=hc0b0e79_3 spades=3.12.0=py36_0 trimmomatic=0.38=1 samtools=1.6=0 picard=2.18.26=0 bwa==0.7.15=1
+conda install -y -c bioconda blast=2.5.0=hc0b0e79_3 spades=3.12.0=py36_0 trimmomatic=0.38=1 samtools=1.6=0 picard=2.18.26=0 bwa==0.7.15=1 quast=5.0.1=py27pl526ha92aebf_0
 if [[ $type = "prod" ]]; then
-    pip install -r requirements.txt --no-cache-dir && pip install .
+    pip install -r requirements.txt && pip install .
 elif [[ $type = "dev" ]] || [[ $type = "stage" ]]; then
-    pip install -r requirements.txt --no-cache-dir && pip install -e .
+    pip install -r requirements.txt && pip install -e .
 fi
 echo "Installation Complete!"
 while true; do
