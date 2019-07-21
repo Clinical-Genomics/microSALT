d364fc8f11a6f037e584f40b5cb25826063cb44e
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2019-07-21 12:38:11 +0200

    Hotfix: Removal of dev branch broke integrated image

--- microSALT/__init__.py
+++ microSALT/__init__.py
@@ -4,7 +4,7 @@ import sys
 
 from flask import Flask
 
-__version__ = '2.8.5'
+__version__ = '2.8.6'
 
 app = Flask(__name__, template_folder='server/templates')
 app.config.setdefault('SQLALCHEMY_DATABASE_URI', 'sqlite:///:memory:')
