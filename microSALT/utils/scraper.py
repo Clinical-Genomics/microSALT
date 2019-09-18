1fc13954b046530d654225687d3664fa9a3c030e
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2019-09-18 12:53:10 +0200

    Fixed scraper issue

--- microSALT/utils/scraper.py
+++ microSALT/utils/scraper.py
@@ -148,7 +148,7 @@ class Scraper():
 
     try:
       for file in q_list:
-        filename = file.rsplit('.',1)[0] #Removes suffix
+        filename = os.path.basename(file).rsplit('.',1)[0] #Removes suffix
         if filename == 'lactam':
           filename = 'beta-lactam'
         if type == 'resistance':
