f7037cc47256a5a6d1219847c3bcd13ab3acb46c
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2020-01-14 09:08:51 +0100

    Resolved issue with unset config var during installation

--- microSALT/__init__.py
+++ microSALT/__init__.py
@@ -20,6 +20,7 @@ wd=os.path.dirname(os.path.realpath(__file__))
 
 # Load configuration
 config = ''
+logger = ''
 default = os.path.join(os.environ['HOME'], '.microSALT/config.json')
 
 if 'MICROSALT_CONFIG' in os.environ:
@@ -37,51 +38,51 @@ elif os.path.exists(default):
   except Exception as e:
     print("Config error: {}".format(str(e))) 
     pass
-# Load flask instance
+
+# Config dependent section:
 if config != '':
   try:
+    #Load flask info
     app.config.update(config['database'])
-  except Exception as e:
-    print("Config error: {}".format(str(e)))
-    pass
-else:
-  print("ERROR: No properly set-up config under neither envvar MICROSALT_CONFIG nor ~/.microSALT/config.json. Exiting.")
-  sys.exit(-1)
 
-#Add extrapaths to config
-config['folders']['expac'] = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'unique_references/EXPAC.fsa'))
-#Check if release install exists
-for entry in os.listdir(get_python_lib()): 
-  if 'microSALT-' in entry:
-    config['folders']['expac'] = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'expac/EXPAC.fsa'))
-    break
-config['folders']['adapters'] = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'share/trimmomatic-0.39-1/adapters/'))
+    #Add extrapaths to config
+    config['folders']['expac'] = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'unique_references/EXPAC.fsa'))
+    #Check if release install exists
+    for entry in os.listdir(get_python_lib()):
+      if 'microSALT-' in entry:
+        config['folders']['expac'] = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'expac/EXPAC.fsa'))
+        break
+    config['folders']['adapters'] = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'share/trimmomatic-0.39-1/adapters/'))
 
-#Initialize logger
-logger = logging.getLogger('main_logger')
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(os.path.expanduser(config['folders']['log_file']))
-fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-logger.addHandler(fh)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
-logger.addHandler(ch)
+    #Initialize logger
+    logger = logging.getLogger('main_logger')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(os.path.expanduser(config['folders']['log_file']))
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(fh)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+    logger.addHandler(ch)
 
-#Create paths mentioned in config
-for entry in config.keys():
-  if entry != '_comment':
-    if isinstance(config[entry], str) and '/' in config[entry] and entry not in ['database', 'genologics']:
-      unmade_fldr = config[entry]
-      if not pathlib.Path(unmade_fldr).exists():
-        os.makedirs(unmade_fldr)
-        logger.info("Created path {}".format(unmade_fldr))
-
-    #level two
-    elif isinstance(config[entry], collections.Mapping):
-      for thing in config[entry].keys():
-        if isinstance(config[entry][thing], str) and '/' in config[entry][thing] and entry not in ['database', 'genologics']:
-          unmade_fldr = config[entry][thing]
+    #Create paths mentioned in config
+    for entry in config.keys():
+      if entry != '_comment':
+        if isinstance(config[entry], str) and '/' in config[entry] and entry not in ['database', 'genologics']:
+          unmade_fldr = config[entry]
           if not pathlib.Path(unmade_fldr).exists():
             os.makedirs(unmade_fldr)
             logger.info("Created path {}".format(unmade_fldr))
+
+        #level two
+        elif isinstance(config[entry], collections.Mapping):
+          for thing in config[entry].keys():
+            if isinstance(config[entry][thing], str) and '/' in config[entry][thing] and entry not in ['database', 'genologics']:
+              unmade_fldr = config[entry][thing]
+              if not pathlib.Path(unmade_fldr).exists():
+                os.makedirs(unmade_fldr)
+                logger.info("Created path {}".format(unmade_fldr))
+
+  except Exception as e:
+    print("Config error: {}".format(str(e)))
+    pass
