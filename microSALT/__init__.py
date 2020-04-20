9d6b2c4da6adf56c7685377fdd61b25c27f34101
Isak Sylvin <isak.sylvin@scilifelab.se>
2020-04-20 11:20:51 +0200

    Update __init__.py

--- microSALT/__init__.py
+++ microSALT/__init__.py
@@ -10,7 +10,7 @@ import sys
 from flask import Flask
 from distutils.sysconfig import get_python_lib
 
-__version__ = '2.9.2'
+__version__ = '2.9.3'
 
 app = Flask(__name__, template_folder='server/templates')
 app.config.setdefault('SQLALCHEMY_DATABASE_URI', 'sqlite:///:memory:')
