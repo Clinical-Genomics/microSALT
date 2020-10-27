357c2b2e4acbd8cc915a178ae5dc22acb494d5b0
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2020-10-27 13:47:44 +0100

    Refactored mock code

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -18,13 +18,34 @@ from microSALT.utils.reporter import Reporter
 from microSALT.utils.referencer import Referencer
 from microSALT.store.db_manipulator import DB_Manipulator
 
-def date_hook(json_dict):
-    for (key, value) in json_dict.items():
-        try:
-            json_dict[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
-        except:
-            pass
-    return json_dict
+def unpack_db_json(filename):
+  testdata = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/{}'.format(filename)))
+  #Check if release install exists
+  for entry in os.listdir(get_python_lib()):
+    if 'microSALT-' in entry:
+      testdata = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/{}'.format(filename)))
+  with open(testdata) as json_file:
+    data = json.load(json_file)
+  return data
+
+@pytest.fixture
+def mock_db():
+  db_file = re.search('sqlite:///(.+)', preset_config['database']['SQLALCHEMY_DATABASE_URI']).group(1)
+  dbm = DB_Manipulator(config=preset_config,log=logger)
+  dbm.create_tables()
+
+  for antry in unpack_db_json('sampleinfo_projects.json'):
+    dbm.add_rec(antry, 'Projects')
+  for entry in unpack_db_json('sampleinfo_mlst.json'):
+    dbm.add_rec(entry, 'Seq_types')
+  for bentry in unpack_db_json('sampleinfo_resistance.json'):
+    dbm.add_rec(bentry, 'Resistances')
+  for centry in unpack_db_json('sampleinfo_expec.json'):
+    dbm.add_rec(centry, 'Expacs')
+  for dentry in unpack_db_json('sampleinfo_reports.json'):
+    dbm.add_rec(dentry, 'Reports')
+
+  return dbm
 
 @pytest.fixture
 def reference_data():
@@ -38,124 +59,11 @@ def reference_data():
   return data
 
 @pytest.fixture
-def mlst_data():
-  pfile = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/sampleinfo_mlst.json'))
-  #Check if release install exists
-  for entry in os.listdir(get_python_lib()):
-    if 'microSALT-' in entry:
-      pfile = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/sampleinfo_mlst.json'))
-  try:
-    with open(pfile) as json_file:
-      data = json.load(json_file)
-  except Exception as e:
-    print("Unable to read provided sample info file as json. Exiting..")
-    sys.exit(-1)
-  return data
-
-@pytest.fixture
-def resistance_data():
-  pfile = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/sampleinfo_resistance.json'))
-  #Check if release install exists
-  for entry in os.listdir(get_python_lib()): 
-    if 'microSALT-' in entry:
-      pfile = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/sampleinfo_resistance.json'))
-  try:
-    with open(pfile) as json_file:
-      data = json.load(json_file)
-  except Exception as e:
-    print("Unable to read provided sample info file as json. Exiting..")
-    sys.exit(-1)
-  return data
-
-@pytest.fixture
-def expec_data():
-  pfile = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/sampleinfo_expec.json'))
-  #Check if release install exists
-  for entry in os.listdir(get_python_lib()): 
-    if 'microSALT-' in entry:
-      pfile = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/sampleinfo_expec.json'))
-  try:
-    with open(pfile) as json_file:
-      data = json.load(json_file)
-  except Exception as e:
-    print("Unable to read provided sample info file as json. Exiting..")
-    sys.exit(-1)
-  return data
-
-@pytest.fixture
-def report_data():
-  pfile = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/sampleinfo_reports.json'))
-  #Check if release install exists
-  for entry in os.listdir(get_python_lib()):
-    if 'microSALT-' in entry:
-      pfile = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/sampleinfo_reports.json'))
-  try:
-    with open(pfile) as json_file:
-      data = json.load(json_file)
-      #xata = json.load(json_file)
-      #data = list()
-      #for entry in xata:
-      #  data.append( json.loads(json.dumps(entry), object_hook=date_hook ))
-  except Exception as e:
-    print("Unable to read provided sample info file as json. Exiting..")
-    sys.exit(-1)
-  return data
-
-@pytest.fixture
-def project_data():
-  pfile = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.parent, 'tests/testdata/sampleinfo_projects.json'))
-  #Check if release install exists
-  for entry in os.listdir(get_python_lib()):
-    if 'microSALT-' in entry:
-      pfile = os.path.abspath(os.path.join(os.path.expandvars('$CONDA_PREFIX'), 'testdata/sampleinfo_projects.json'))
-  try:
-    with open(pfile) as json_file:
-      data = json.load(json_file, object_hook=date_hook)
-  except Exception as e:
-    print("Unable to read provided sample info file as json. Exiting..")
-    sys.exit(-1)
-  return data
-
-@pytest.fixture
-def dbm():
-  db_file = re.search('sqlite:///(.+)', preset_config['database']['SQLALCHEMY_DATABASE_URI']).group(1)
-  dbm = DB_Manipulator(config=preset_config,log=logger)
-  dbm.create_tables()
-
-  assert dbm.engine.dialect.has_table(dbm.engine, 'samples')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'seq_types')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'resistances')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'expacs')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'projects')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'reports')
-  assert dbm.engine.dialect.has_table(dbm.engine, 'collections')
-  return dbm
-
-@pytest.fixture
-def init_database(dbm, mlst_data, resistance_data, expec_data, report_data, project_data):
-  for xentry in project_data:
-    dbm.add_rec(xentry, 'Projects')
-  for entry in mlst_data:
-    dbm.add_rec(entry, 'Seq_types')
-  for bentry in resistance_data:
-    dbm.add_rec(bentry, 'Resistances')
-  for centry in expec_data:
-    dbm.add_rec(centry, 'Expacs')
-  for dentry in report_data:
-    dbm.add_rec(dentry, 'Reports')
-
-@pytest.fixture
-def init_references(reference_data):
-  ref_obj = Referencer(config=preset_config, log=logger, sampleinfo=reference_data)
-  ref_obj.identify_new(reference_data[0].get('CG_ID_project'),project=True)
-  ref_obj.update_refs()
-
-@pytest.fixture
 def reporter(reference_data):
   reporter_obj = Reporter(config=preset_config, log=logger, sampleinfo=reference_data[0], name="MIC1234A1", output="/tmp/MLST")
   return reporter_obj
 
-def test_motif(init_database, reporter):
+def test_motif(mock_db, reporter):
   reporter.create_subfolders()
   reporter.gen_motif(motif="resistance")
   assert len( glob.glob("{}/AAA1234_resistance*".format(reporter.output))) > 0
@@ -163,12 +71,12 @@ def test_motif(init_database, reporter):
   reporter.gen_motif(motif="expec")
   assert len( glob.glob("{}/AAA1234_expec*".format(reporter.output))) > 0
 
-def test_deliveryreport(init_database, reporter):
+def test_deliveryreport(mock_db, reporter):
   reporter.create_subfolders()
   reporter.gen_delivery()
   assert len( glob.glob("{}/deliverables/999999_deliverables.yaml".format(preset_config['folders']['reports']))) > 0
 
-def test_jsonreport(init_database, reporter):
+def test_jsonreport(mock_db, reporter):
   reporter.create_subfolders()
   reporter.gen_json()
   assert len( glob.glob("{}/json/AAA1234.json".format(preset_config['folders']['reports']))) > 0
