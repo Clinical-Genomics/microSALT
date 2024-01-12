d4d9f611a2ed1b4be3a3c5205199d8cf0c8ea4ad
talnor <tanja.normark@scilifelab.se>
2024-01-12 15:39:46 +0100

    install from yml

--- install.sh
+++ install.sh
@@ -56,21 +56,8 @@ conda info | tac | tac | grep -q $cname && conda deactivate || :
 #Remove environment if already present
 conda remove -y -n $cname --all || :
 
-conda create -y -n $cname python=3.6
+conda env create -y -n $cname -f https://raw.githubusercontent.com/Clinical-Genomics/microSALT/$branch/environment.yml
 conda activate $cname
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda install -y blast=2.12.0
-conda install -y bwa=0.7.17
-conda install -y picard=2.20.3
-conda install -y pigz=2.4
-conda install -y quast=5.0.2
-conda install -y samtools=1.13
-conda install -y spades=3.13.1
-conda install -y trimmomatic=0.39
-conda install -y r-base=4.1.1
-conda install -y openjdk=11.0.9.1
 
 if [[ $type == "release" ]]; then
     pip install -r https://raw.githubusercontent.com/Clinical-Genomics/microSALT/$branch/requirements.txt -r https://raw.githubusercontent.com/Clinical-Genomics/microSALT/$branch/requirements-dev.txt 
