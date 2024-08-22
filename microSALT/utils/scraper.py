1ab5a51bf28941a1b0f86e5b7d54c2c93012bd85
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-08-22 13:07:04 +0200

    149 fix missing resistances (#179)
    * Add more blast hits in resistance test data
    
    * Return hits in scraping method and fail test by requiring blaOXA-48
    
    * Fix #149 by making regex specific and not matching multiple genes

--- microSALT/utils/scraper.py
+++ microSALT/utils/scraper.py
@@ -257,9 +257,9 @@ class Scraper:
                                         ].capitalize()
                                     #Ignores reference name and finds relevant resFinder entry
 
-                                    padder = [x for x in locilengths.keys() if x.startswith('>{}'.format(partials[1]))]
+                                    padder = [x for x in locilengths.keys() if x.startswith('>{}_'.format(partials[1]))]
                                     if len(padder) == 0:
-                                        padder = [x for x in locilengths.keys() if x.startswith('>{}'.format(partials[1][:-1]))]
+                                        padder = [x for x in locilengths.keys() if x.startswith('>{}_'.format(partials[1][:-1]))]
                                     try:
                                         padder = padder[0]
                                     except IndexError as e:
@@ -438,6 +438,7 @@ class Scraper:
                         self.name, str(e)
                     )
                 )
+        return hypo
 
     def load_resistances(self):
         """Legacy function, loads common resistance names for genes from notes file"""
