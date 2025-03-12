a8517ac803a234fab9e7d4671edda0ef44d2e1f4
karlnyr <kallejohan211@hotmail.com>
2025-03-12 10:25:36 +0100

    Refactor referencer utilities to improve species identification and reference retrieval

--- microSALT/utils/referencer/utils.py
+++ microSALT/utils/referencer/utils.py
@@ -1,30 +1,38 @@
-from microSALT.utils.referencer.constants import Escherichia, Klebsiella
+from microSALT.utils.referencer.constants import (
+    Escherichia,
+    Klebsiella,
+    ESCHERICHIA_REFERENCE,
+    KLEBSIELLA_REFERENCE,
+)
 
 
-def is_escherichia(organism_name: str) -> bool:
+def get_species(organism_name: str) -> str:
+    """Returns the species of the organism name."""
+    return organism_name.lower().split()[-1]
+
+
+def is_escherichia(species_name: str) -> bool:
     """Checks if the organism name is a proper Escherichia."""
-    organism_name = organism_name.lower()
-    return any(organism_name == member.value for member in Escherichia)
+    return any(species_name == member.value for member in Escherichia)
 
 
-def is_klebsiella(organism_name: str) -> bool:
+def is_klebsiella(species_name: str) -> bool:
     """Checks if the organism name is a proper Klebsiella."""
-    organism_name = organism_name.lower()
-    return any(organism_name == member.value for member in Klebsiella)
+    return any(species_name == member.value for member in Klebsiella)
 
 
 def get_reference_if_enterobacteriaceae(organism_name: str) -> str:
     """Returns the reference for the organism name if it belongs to Enterobacteriaceae."""
+    species: str = get_species(organism_name)
     GENUS_REFERENCE_MAP = {
-        "escherichia": (is_escherichia, Escherichia.ESCHERICHIA_REFERENCE),
-        "klebsiella": (is_klebsiella, Klebsiella.KLEBSIELLA_REFERENCE),
+        "escherichia": (is_escherichia, ESCHERICHIA_REFERENCE),
+        "klebsiella": (is_klebsiella, KLEBSIELLA_REFERENCE),
     }
-    organism_name = organism_name.lower()
     return next(
         (
             reference
             for check_func, reference in GENUS_REFERENCE_MAP.values()
-            if check_func(organism_name)
+            if check_func(species_name=species)
         ),
         organism_name,
     )
