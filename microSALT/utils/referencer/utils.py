a703f584a8d8210e5fa753e8e210df392d56a790
karlnyr <kallejohan211@hotmail.com>
2025-03-18 16:26:59 +0100

    fix logic

--- microSALT/utils/referencer/utils.py
+++ microSALT/utils/referencer/utils.py
@@ -16,6 +16,11 @@ def get_genus(organism_name: str) -> str:
     return organism_name.lower().split()[0]
 
 
+def is_enterobacteriaceae(organism_name: str) -> bool:
+    """Checks if the organism name belongs to Enterobacteriaceae."""
+    return get_genus(organism_name) in ("escherichia", "klebsiella")
+
+
 def is_escherichia(species_name) -> bool:
     """Checks if the organism name is a proper Escherichia."""
     return any(species_name == member.value for member in Escherichia)
@@ -29,17 +34,13 @@ def is_klebsiella(species_name) -> bool:
 def get_reference_if_enterobacteriaceae(organism_name: str) -> str:
     """Returns the reference for the organism name if it belongs to Enterobacteriaceae."""
     GENUS_REFERENCE_MAP = {
-        "escherichia": (is_escherichia, ESCHERICHIA_REFERENCE),
-        "klebsiella": (is_klebsiella, KLEBSIELLA_REFERENCE),
+        (is_escherichia, ESCHERICHIA_REFERENCE),
+        (is_klebsiella, KLEBSIELLA_REFERENCE),
     }
 
     species: str = get_species(organism_name)
-    genus = get_genus(organism_name)
-
-    is_species, reference = GENUS_REFERENCE_MAP.get(
-        genus, (None, None)
-    )  # if the genus is not in the map, return None, None
-
-    return (
-        reference if is_species(species_name=species) else organism_name
-    )  # if the species is not in the species for the genus, return the organism name
+    if is_enterobacteriaceae(organism_name):
+        for is_species, reference in GENUS_REFERENCE_MAP:
+            if is_species(species):
+                return reference
+    return organism_name
