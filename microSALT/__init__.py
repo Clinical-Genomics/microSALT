f547533cd6101e71493f96a55a9262c0cd559c87
karlnyr <kallejohan211@hotmail.com>
2025-03-31 10:17:41 +0200

    revert logging level to info

--- microSALT/__init__.py
+++ microSALT/__init__.py
@@ -80,9 +80,9 @@ if preset_config != "":
 
         # Initialize logger
         logger = logging.getLogger("main_logger")
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(logging.INFO)
         ch.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
         logger.addHandler(ch)
 
