b268cafb88a16a3833b66230c3abc96b4226a6a3
karlnyr <kallejohan211@hotmail.com>
2026-02-23 13:14:26 +0100

    major update

--- microSALT/__init__.py
+++ microSALT/__init__.py
@@ -12,7 +12,7 @@ from logging import Logger
 
 from flask import Flask
 
-__version__ = "4.2.8"
+__version__ = "5.0.0"
 
 app = Flask(__name__, template_folder="server/templates")
 app.config.setdefault("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
