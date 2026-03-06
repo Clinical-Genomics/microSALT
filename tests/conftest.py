b86ffd9087e71b4b3cac73000645b4506a8234dc
karlnyr <kallejohan211@hotmail.com>
2026-03-06 14:03:23 +0100

    update fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,7 +52,6 @@ def exp_config():
             "resistances",
             "genomes",
             "expec",
-            "adapters",
             "credentials",
         },
         "threshold": {
@@ -82,4 +81,6 @@ def exp_config():
         "database": {"SQLALCHEMY_DATABASE_URI", "SQLALCHEMY_TRACK_MODIFICATIONS", "DEBUG"},
         "pubmlst": {"client_id", "client_secret"},
         "pasteur": {"client_id", "client_secret"},
+        "singularity": {"binary", "bind_paths", "trimmomatic_adapters"},
+        "containers": {"skesa", "blast", "bwa", "samtools", "picard", "trimmomatic", "quast"},
     }
