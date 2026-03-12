6af60744cc4f74718e7a2d2a70fd1cec4a92abaf
karlnyr <kallejohan211@hotmail.com>
2026-03-12 15:15:40 +0100

    update orm models

--- microSALT/store/orm_models.py
+++ microSALT/store/orm_models.py
@@ -24,9 +24,9 @@ class Samples(Base):
     # steps = relationship("Steps", back_populates="samples")
     expacs = relationship("Expacs", back_populates="samples")
 
-    CG_ID_sample = Column(String(15), primary_key=True, nullable=False)
-    CG_ID_project = Column(String(15), ForeignKey("projects.CG_ID_project"))
-    Customer_ID_sample = Column(String(40))
+    CG_ID_sample = Column(String(32), primary_key=True, nullable=False)
+    CG_ID_project = Column(String(32), ForeignKey("projects.CG_ID_project"))
+    Customer_ID_sample = Column(String(128))
     organism = Column(String(100))
     ST = Column(SmallInteger, default=-1)
     pubmlst_ST = Column(SmallInteger, default=-1)
@@ -62,7 +62,7 @@ class Seq_types(Base):
     __tablename__ = "seq_types"
     samples = relationship("Samples", back_populates="seq_types")
 
-    CG_ID_sample = Column(String(15), ForeignKey("samples.CG_ID_sample"), primary_key=True)
+    CG_ID_sample = Column(String(32), ForeignKey("samples.CG_ID_sample"), primary_key=True)
     loci = Column(String(10), primary_key=True)
     allele = Column(SmallInteger)
     contig_name = Column(String(20), primary_key=True)
@@ -82,7 +82,7 @@ class Resistances(Base):
     __tablename__ = "resistances"
     samples = relationship("Samples", back_populates="resistances")
 
-    CG_ID_sample = Column(String(15), ForeignKey("samples.CG_ID_sample"), primary_key=True)
+    CG_ID_sample = Column(String(32), ForeignKey("samples.CG_ID_sample"), primary_key=True)
     gene = Column(String(50), primary_key=True)
     instance = Column(String(30), primary_key=True)
     contig_name = Column(String(20), primary_key=True)
@@ -103,7 +103,7 @@ class Expacs(Base):
     __tablename__ = "expacs"
     samples = relationship("Samples", back_populates="expacs")
 
-    CG_ID_sample = Column(String(15), ForeignKey("samples.CG_ID_sample"), primary_key=True)
+    CG_ID_sample = Column(String(32), ForeignKey("samples.CG_ID_sample"), primary_key=True)
     gene = Column(String(50), primary_key=True)
     instance = Column(String(30), primary_key=True)
     contig_name = Column(String(20), primary_key=True)
@@ -125,10 +125,10 @@ class Projects(Base):
     samples = relationship("Samples", back_populates="projects")
     reports = relationship("Reports", back_populates="projects")
 
-    CG_ID_project = Column(String(15), primary_key=True, nullable=False)
-    Customer_ID_project = Column(String(15))
+    CG_ID_project = Column(String(32), primary_key=True, nullable=False)
+    Customer_ID_project = Column(String(32))
     date_ordered = Column(DateTime)
-    Customer_ID = Column(String(15))
+    Customer_ID = Column(String(32))
 
 
 class Versions(Base):
@@ -143,7 +143,7 @@ class Reports(Base):
     __tablename__ = "reports"
     projects = relationship("Projects", back_populates="reports")
 
-    CG_ID_project = Column(String(15), ForeignKey("projects.CG_ID_project"), primary_key=True)
+    CG_ID_project = Column(String(32), ForeignKey("projects.CG_ID_project"), primary_key=True)
     steps_aggregate = Column(String(100))
     date = Column(DateTime)
     version = Column(Integer, default=1, primary_key=True)
@@ -152,8 +152,8 @@ class Reports(Base):
 class Collections(Base):
     __tablename__ = "collections"
 
-    ID_collection = Column(String(15), primary_key=True)
-    CG_ID_sample = Column(String(15), primary_key=True)
+    ID_collection = Column(String(32), primary_key=True)
+    CG_ID_sample = Column(String(32), primary_key=True)
 
 
 class SystemLock(Base):
